f20b51a39288a23a7394c69f66188c4ebb2a526c
SaeedAbdulrahman <saeed.ibrahim.abdulrahman@gmail.com>
2019-12-31 23:34:27 +0400

    Finished coding the script.

--- age-calc.py
+++ age-calc.py
@@ -1,1 +1,15 @@
+from datetime import date
 import os
+
+os.system('clear')
+
+print("Please input the following: (DOB)\n")
+day = input("Day [1-31]: ")
+month = input("Month [1-12]: ")
+year = input("Year: ")
+
+def calcage(dob):
+    age = int((date.today() - dob).days / 365.2425)
+    return str(age)
+
+print("\nYou are " + calcage(date(int(year), int(month), int(day))) + " years old.")
